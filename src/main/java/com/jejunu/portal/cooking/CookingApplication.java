141b1b1e696930ac0fdb83b45fcb876c2d59c6de
SeungJu Kim <pipiru100@gmail.com>
2019-05-23 22:58:07 +0900

    프로젝트 생

---
+++ src/main/java/com/jejunu/portal/cooking/CookingApplication.java
@@ -0,0 +1,13 @@
+package com.jejunu.portal.cooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingApplication.class, args);
+    }
+
+}
